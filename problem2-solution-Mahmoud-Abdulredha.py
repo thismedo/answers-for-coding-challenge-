b5538e19ba9f719887fdc8de3438e313b7a96381
Mahmoud <84873091+thismedo@users.noreply.github.com>
2021-10-23 01:23:38 +0300

    Create problem2-solution-Mahmoud-Abdulredha.py

--- problem2-solution-Mahmoud-Abdulredha.py
+++ problem2-solution-Mahmoud-Abdulredha.py
@@ -1,7 +1,8 @@
-fridge = {}
-ingredients = {}
+fridge = {"banana": 1, "orange": 2, "mashroom": 3, "apple": 4}
+ingredients = {"apple": 4, "banana": 1}
 
 
 def validateRecipeWithQuantity():
     if ingredients in fridge:
-    
+        return True 
+    else: False
